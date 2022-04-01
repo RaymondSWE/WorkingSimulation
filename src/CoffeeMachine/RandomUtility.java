408358bf36bf687e304479fe460aaef99197b07e
nibarsito <nibarsito2@gmail.com>
2022-04-01 10:39:04 +0200

    fixed a comment that was written wrong

--- src/CoffeeMachine/RandomUtility.java
+++ src/CoffeeMachine/RandomUtility.java
@@ -11,8 +11,7 @@ public class RandomUtility {
 
 //this function takes an integer that is a max number and generates a random number between 0 and the number given.
 	public int getRandomNumber(int min, int max) {
-		// nextInt is normally exclusive of the top value, so we add 1 to make it
-		// inclusive
+		// nextInt is normally exclusive of the top value, so we add 1 to make it inclusive.
 		return random.nextInt(min, max + 1);
 	}
 }
