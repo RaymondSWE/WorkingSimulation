a0a3ef5bf4fc3f19027ffedfac28451704de368c
nibarsito <nibarsito2@gmail.com>
2022-04-01 10:49:26 +0200

    fixed the length of the persons array in fika so it's not hard coded

--- src/CoffeeMachine/Fika.java
+++ src/CoffeeMachine/Fika.java
@@ -1,9 +1,8 @@
 package CoffeeMachine;
-
 import java.util.Random;
 
 public class Fika {
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		/**
 		 * Initiating Coffee Machine
 		 */
@@ -18,22 +17,20 @@ public class Fika {
 		/**
 		 * Randomize names, taken from randomName method.
 		 */
-		RandomUtility random = new RandomUtility();
-		for (int i = 0; i < 4; i++) {
+		RandomUtility random=new RandomUtility();
+		for (int i = 0; i < persons.length; i++) {
 			persons[i] = new Person(randomName(random), drink, 30, 90);
 			Thread personThreads = new Thread(persons[i]);
 			personThreads.start();
 		}
-
-	}
-
+		
+	} 
 	public static String randomName(RandomUtility random) {
-		String[] firstTwo = { "Te", "Le", "Ma", "Si", "Pa", "Ro", "Ed", "Fu", "Ca", "Ka", "Ri", "Ev", "Pe", "Ze",
-				"Co" };
-		String[] lastTwo = { "ta", "xi", "ri", "ny", "er", "bu", "an", "ck", "rl", "ll", "on", "rt", "ra", "sa", "ta" };
+		String[] firstTwo = { "Te", "Le", "Ma", "Si", "Pa", "Ro", "Ed", "Fu", "Ca", "Ka", "Ri", "Ev", "Pe", "Ze", "Co"};
+		String[] lastTwo = { "ta", "xi", "ri", "ny", "er", "bu", "an", "ck", "rl", "ll", "on", "rt", "ra", "sa", "ta"};
 
 		String selectFirstTwo = firstTwo[random.getRandomNumber(0, firstTwo.length)];
-		String selectLastTwo = lastTwo[random.getRandomNumber(0, lastTwo.length - 1)];
+		String selectLastTwo = lastTwo[random.getRandomNumber(0, lastTwo.length-1)];
 		return selectFirstTwo + selectLastTwo;
 	}
 
